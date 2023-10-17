ed07ebe561b0dce4b10e24b4e2fafaf24052dbc5
parsapanahpoor <parsapanahpoor77@gmail.com>
2023-10-17 11:49:07 +0330

    complete step 1

---
+++ Ref-keyword/Ref-keyword/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ref_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            int b = 20;
+
+            PlusNumber1(a);//Will Print 21
+            Console.WriteLine(a);//Will Print 20
+
+            PlusNumber2(ref b);//Will Print 21
+            Console.WriteLine(b);//Will Print 21
+
+            Console.ReadKey();
+        }
+
+        public static void PlusNumber1(int a)
+        {
+            a++;
+            Console.WriteLine(a);
+        }
+
+        public static void PlusNumber2(ref int b)
+        { 
+            b++;
+            Console.WriteLine(b);
+        }
+    }
+}
